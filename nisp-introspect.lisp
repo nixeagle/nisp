f09a2eda2973eb014281c6013546bdcc3a830c3d
James S <james@nixeagle.org>
2009-12-06 19:53:30 +0000

    remove some extra defmethods not needed

--- nisp-introspect.lisp
+++ nisp-introspect.lisp
@@ -1,28 +1,40 @@
 ;;; Introspection of lisp systems. May be portable one day.
 (in-package :nisp-introspect)
 
+(def-suite nisp-introspect-suite
+    :in nisp::all-tests)
+(in-suite nisp-introspect-suite)
 
 (defgeneric function-lambda-list (symbol)
-  (:method
-      :around (symbol)
-      (handler-case
-          (call-next-method)
-        (error () (format nil "Passed input type ~A not valid extended function designator." (type-of symbol)))))
+  (:method (symbol)
+    (sb-introspect:function-lambda-list symbol))
   (:documentation
-   "Return lambda list description given a string or a valid function
-   designator."))
+   "Return lambda list description given a string or a valid function designator."))
 
-(defmethod function-lambda-list ((symbol function))
-  
-  #+sbcl (sb-introspect:function-lambda-list symbol)
-  #-sbcl (error)
-  )
+(defmethod function-lambda-list :around (symbol)
+  "Handle errors originating from a function-lambda list call."
+  (handler-case (call-next-method)
+        (error ()
+          (format nil "Passed input type ~A not valid extended function designator."
+                  (type-of symbol)))))
 
 (defmethod function-lambda-list ((symbol string))
-  (function-lambda-list (intern symbol)))
+  "If we get a string, read it and re-call."
+  (function-lambda-list (read-from-string symbol)))
 
-(defmethod function-lambda-list ((symbol symbol))
-  (unless (fboundp symbol)
-    (error 'error symbol))
-  (function-lambda-list (symbol-function symbol)))
+(test function-lambda-list/string
+  "Should return a lambda list of the function's arguments"
+  (is (equal '(&REST SB-KERNEL::ARGS)
+             (function-lambda-list "+"))
+      "+ has always worked, but this is the base case.")
+  (is (equal '(&REST SB-IMPL::ARGS)
+             (function-lambda-list "list"))
+      "list is a function, we need to read it as such.")
+  (is (stringp (function-lambda-list "1"))
+      "Not a function, invalid input"))
 
+(test function-lambda-list/symbol
+  "Anything other then a functionp input should return error string."
+  (is (equal '(&REST SB-KERNEL::ARGS)
+             (function-lambda-list '+)))
+  (is (stringp (function-lambda-list 1))))
