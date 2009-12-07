9f1147e2927c80ff116c838fb94a55492328ab02
James S <james@nixeagle.org>
2009-12-07 17:38:37 +0000

    more tests, one still fails plus add a few random emacs interaction functions, used with slime. I'll add the elisp ones later.

--- package.lisp
+++ package.lisp
@@ -44,6 +44,34 @@
       "Expect a list")
   (is (listp (list-system-components '(:nisp :nispbot)))
       "Should be able to take more then one arg.")
-  (signals type-error (list-system-components 1))
-)
+  (signals (simple-type-error) (list-system-components 1))
+  (signals (simple-type-error) (list-system-components nil))
+  (is (listp (list-system-components "nisp"))))
+ 
 
+(defpackage #:nisp-random
+  (:use :cl :5am))
+(in-package :nisp-random)
+
+;;; source taken from swank and modified to do what I wanted to do.
+
+(defun nisp-pprint-eval (string)
+  (swank::with-buffer-syntax ()
+    (let* ((stand (make-string-output-stream))
+           (trace (make-string-output-stream))
+           (*standard-output* stand)
+           (*trace-output* trace)
+           (form (read-from-string string))
+           (values (multiple-value-list
+                    (eval form))))
+      (list string
+            (get-output-stream-string stand)
+            (get-output-stream-string trace)
+            (nisp-pprint values)))))
+
+(defun nisp-pprint (values)
+  (swank::with-buffer-syntax ()
+    (swank::with-bindings swank::*swank-pprint-bindings*
+      (with-output-to-string (*standard-output*)
+        (dolist (o values)
+          (pprint o))))))
