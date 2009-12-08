3a4940744cc204074978750688448fe02055a15b
James S <james@nixeagle.org>
2009-12-08 03:27:54 +0000

    remove the (progn not useful and caused def-fixture to be evaulated in the wrong scope

--- 5amfix.lisp
+++ 5amfix.lisp
@@ -1,12 +1,12 @@
 ;;; Redefine 5am's def-fixture so its not broken.
-#+5am
-(progn
-  (in-package :FiveAM)
 
-  (setq *debug-on-error* nil)
 
-  (defmacro def-fixture (name args &body body)
-    "Defines a fixture named NAME. A fixture is very much like a
+(in-package :FiveAM)
+
+(setq 5am:*debug-on-error* nil)
+
+(defmacro def-fixture (name args &body body)
+  "Defines a fixture named NAME. A fixture is very much like a
 macro but is used only for simple templating. A fixture created
 with DEF-FIXTURE is a macro which can use the special macrolet
 &BODY to specify where the body should go.
@@ -16,7 +16,7 @@ See Also: WITH-FIXTURE
 *note* that this overrides the default 5am DEF-FIXTURE. 5am's
 DEF-FIXTURE evals itself too much causing a SBCL compiler warning
 about redefining a symbol."
-    `(eval-when (:compile-toplevel :load-toplevel :execute)
-       (rem-fixture ',name)
-       (setf (get-fixture ',name) (cons ',args ',body))
-       ',name)))
+  `(eval-when (:compile-toplevel :load-toplevel :execute)
+     (rem-fixture ',name)
+     (setf (get-fixture ',name) (cons ',args ',body))
+     ',name))
