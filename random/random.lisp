b6b6af11fafecfd9f2922fdf41d4515edf7a7345
James S <james@nixeagle.org>
2009-12-08 01:15:05 +0000

    put correct code in random

--- random/random.lisp
+++ random/random.lisp
@@ -1,15 +1,22 @@
+(defpackage #:nisp-random
+  (:use :cl :5am))
 (in-package :nisp-random)
 
 ;;; source taken from swank and modified to do what I wanted to do.
 
 (defun nisp-pprint-eval (string)
-  (let* ((stand (make-string-output-stream))
-         (trace (make-string-output-stream))
-         (*standard-output* stand)
-         (*trace-output* trace)
-         (values (multiple-value-list
-                  (eval (read-from-string string)))))
-    (list (get-output-stream-string stand) (nisp-pprint values))))
+  (swank::with-buffer-syntax ()
+    (let* ((stand (make-string-output-stream))
+           (trace (make-string-output-stream))
+           (*standard-output* stand)
+           (*trace-output* trace)
+           (form (read-from-string string))
+           (values (multiple-value-list
+                    (eval form))))
+      (list string
+            (get-output-stream-string stand)
+            (get-output-stream-string trace)
+            (nisp-pprint values)))))
 
 (defun nisp-pprint (values)
   (swank::with-buffer-syntax ()
