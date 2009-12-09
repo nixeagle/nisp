94db7af42d7733d8ee5dbb09fd2b296c9fbee11a
James S <james@nixeagle.org>
2009-12-09 20:27:17 +0000

    Remove emacs specific functions for the time being, they don't belong here and since I switched my slime keybindings to use the 'random' stuff, these functions need to be loaded *before* I load in the nisp system using emacs.
    I still plan on releasing a minor mode with this stuff, but its going to have to be a seperate package from nisp and likely won't get done for a a week or so.

--- random/random.lisp
+++ random/random.lisp
@@ -4,31 +4,6 @@
 
 ;;; source taken from swank and modified to do what I wanted to do.
 
-(defun nisp-pprint-eval (string &optional extra)
-  (swank::with-buffer-syntax ()
-    (let* ((5am:*run-test-when-defined* extra)
-           (stand (make-string-output-stream))
-           (trace (make-string-output-stream))
-           (err (make-string-output-stream))
-           (*standard-output* stand)
-           (*trace-output* trace)
-           (*error-output* err)
-           (form (read-from-string string))
-           (values (multiple-value-list
-                    (eval form))))
-      (list string
-            (get-output-stream-string stand)
-            (get-output-stream-string err)
-            (get-output-stream-string trace)
-            (nisp-pprint values)))))
-
-(defun nisp-pprint (values)
-  (swank::with-buffer-syntax ()
-    (swank::with-bindings swank::*swank-pprint-bindings*
-      (with-output-to-string (*standard-output*)
-        (dolist (o values)
-          (pprint o))))))
-
 (defun matches-list (reg &rest strings)
   (let ((ret ()))
     (dolist (str strings ret)
