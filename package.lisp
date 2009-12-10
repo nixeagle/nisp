83057b6028916ac5c16c86d5524a028e5c2499e0
James S <james@nixeagle.org>
2009-12-10 07:48:24 +0000

    use lift as a part of all the packages in package.lisp

--- package.lisp
+++ package.lisp
@@ -1,14 +1,14 @@
 
 
 (defpackage #:nisp-asdf
-  (:use :cl #+5am :5am))
+  (:use :cl #+5am :5am :lift))
 
 (defpackage #:nisp-introspect
-  (:use :cl #+5am :5am)
+  (:use :cl #+5am :5am :lift)
   (:export #:function-lambda-list))
 
 
 (defpackage #:nispbot-config
-  (:use :common-lisp #+5am :5am
+  (:use :common-lisp #+5am :5am :lift
         )
   (:export *channel* *nickname* *eighthbit*))
