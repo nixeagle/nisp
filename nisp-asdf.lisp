ff2190c4cf2c5b3de31593b9e88a72b62b767fd3
James S <james@nixeagle.org>
2009-12-10 17:20:21 +0000

    redo tests with lift

--- nisp-asdf.lisp
+++ nisp-asdf.lisp
@@ -20,7 +20,34 @@
 (defmethod list-system-components ((system-name string))
   (asdf:module-components (asdf:find-system system-name)))
 
+(deftestsuite list-system-components (nisp-asdf-suite)
+  ()
+  :test (pass-keyword-with-valid-package-name
+         (ensure-same (integerp (list-system-components :nisp)) t))
+  :test (pass-string-with-valid-package-name
+         (ensure (listp (list-system-components "nisp"))))
+  :test (pass-multiple-args-in-a-list
+         (ensure (listp (list-system-components '(:nisp :nispbot)))))
+  :test (pass-integer
+         (ensure-condition 'simple-type-error
+           (list-system-components 1)))
+  :test (pass-nil
+         (ensure-condition 'asdf::missing-component
+           (list-system-components nil)))
+  (:test 
+    (pass-void-package-keyword
+     (:documentation "testing")
+     (ensure-condition 'asdf::missing-component
+       (list-system-components :this-does-not-exist-as-a-package)
+       ) )
+    )
+  :documentation "list-system-components is a generic function.
 
+The point of all the tests in this suite is to verify that the function
+handles message passing as expected. We verify that strings, lists and
+keywords all return valid results.")
+
+#+ (or)
 (test list-system-components
   (is (listp (list-system-components :nisp))
       "Expect a list")
