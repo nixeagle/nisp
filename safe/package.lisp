7f6db682fc42281f67c414c753929e69b0ae8d44
James S <james@nixeagle.org>
2009-12-10 01:03:03 +0000

    export make-empty-package as well

--- safe/package.lisp
+++ safe/package.lisp
@@ -1,9 +1,11 @@
 (defpackage #:nisp-empty-package
   (:use :cl #+5am :5am)
-  (:export :with-empty-package))
+  (:export #:with-empty-package
+           #:make-empty-package))
 
 (defpackage #:nisp-safe
   (:use :common-lisp #+5am :5am
+        :nisp-empty-package
         :nisp-util))
 
 (in-package :nisp-safe)
