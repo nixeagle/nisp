57ca656085bf529e5ad9bffeb3f6f97ab317027f
James S <james@nixeagle.org>
2009-12-14 08:18:00 +0000

    4 testcases on range, got to play with random testing!

--- safe/safe-tests.lisp
+++ safe/safe-tests.lisp
@@ -68,3 +68,45 @@ AGAIN DO NOT EVEN THINK ABOUT USING WHILE THIS TEST FAILS!")
   :test (expect-error
          (ensure-condition 'simple-error
                         (colon-reader nil nil))))
+
+(defpackage #:safe-external-tests
+  (:use :lift)
+  (:documentation "This package is largely reserved for testing "))
+
+(use-package *prepared-safe-packages* (find-package :safe-external-tests))
+(in-package :safe-external-tests)
+
+
+(deftestsuite root-suite (nisp::root-suite)
+  ()
+  (:documentation "The root of all external (sandbox accessable) function tests."))
+
+(deftestsuite test-range (root-suite)
+  ()
+  (:function
+   (run-range (start end)
+              (ensure (cl:listp (range start end)))
+              (ensure (cl:< 0 (cl:list-length (range start end))))))
+  (:test (count-up
+          (:documentation "The usual case, counting from START to END")
+          (ensure-same (range 0 10)
+                       (list 0 1 2 3 4 5 6 7 8 9 10))))
+  (:test (count-down
+          (:documentation
+           "A tad unusual, but we expect given a START greater then END
+to count _down_ from START to END.")
+          (ensure-same (range 5 -5)
+                       (list 5 4 3 2 1 0 -1 -2 -3 -4 -5))))
+  (:test (random-integer-combos
+          (:documentation "Playing around with random test cases. These
+are largely undocumented in the lift test framework.")
+          (lift:ensure-random-cases 10
+              ((start an-integer)
+               (end an-integer))
+            (run-range start end))))
+  (:test (random-float-combos
+          (:documentation "Make sure that no combination of floats break range.")
+          (lift:ensure-random-cases 10
+              ((start a-double-float)
+               (end a-single-float))
+            (run-range start end)))))
