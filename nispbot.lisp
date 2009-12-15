839185f078cfde950d6b179f0c5d36530ef40faf
James S <james@nixeagle.org>
2009-12-15 02:37:34 +0000

    move bot tests to its own file

--- nispbot.lisp
+++ nispbot.lisp
@@ -11,10 +11,7 @@
 
 (in-package :nispbot)
 
-(deftestsuite root-suite (nisp::root-suite) ())
-(deftestsuite basic-irc-suite (root-suite) ())
-
-
+;;; cl-ppcre registers. Do we need this? --nixeagle
 (setq *allow-named-registers* t)
 
 (defvar *connection*)
@@ -121,29 +118,7 @@ Note that the newline is not replaced by a space!"
       collect char)
    'string))
 
-(deftestsuite strip-newline (root-suite)
-  ()
-  (:test (pass-string
-          (:documentation "Base case")
-          (ensure (stringp (strip-newline "some string"))))))
 
-(deftestsuite test-make-irc-message (basic-irc-suite)
-  ()
-  (:test (format-private-message
-           (:documentation "Demonstrate how to create a private message and
-verify that the result is correct.")
-           (ensure-same (irc::make-irc-message "privmsg" "#channel" "text")
-                        (format nil "privmsg #channel :text~%")
-                        :test #'string=))))
-
-(deftestsuite test-tokenize-string (basic-irc-suite)
-  ()
-  (:documentation "Demonstrate irc::tokenize-string")
-  (:test (split-by-space
-          (ensure-same (irc::tokenize-string
-                        "Some string to test on"
-                        :delimiters '(#\Space))
-                       '("Some" "string" "to" "test" "on")))))
 
 
 (defpackage #:nispbot-basic-commands
