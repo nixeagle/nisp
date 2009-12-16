8e41a58d9668f8fe69c84d305b67c1b6d6601ed5
James S <james@nixeagle.org>
2009-12-16 19:55:37 +0000

    couple notes on *readtable* and move make-safe-package to the top of safe-package.lisp

--- safe/safe-package.lisp
+++ safe/safe-package.lisp
@@ -31,6 +31,14 @@ tracks currently interned packages and whatnot.")
         :initform *prepared-safe-packages*))
   (:documentation "Container class for handling safe packages and doing various operations with them."))
 
+(defun make-safe-package (name)
+  "Should make a totally independent package. All normal safe stuff
+ should be in this by default.
+"
+  (with-package (make-empty-package name)
+    (cl::use-package *prepared-safe-packages*)
+    *package* ) ) 
+
 (defmacro with-safe-package (name &body body)
   "Use the given package name to read some code."
   `(with-package ,name
@@ -96,15 +104,3 @@ The optional OWNER parameter defines who owns the package. There is no restricti
          (intern (concatenate 'string (symbol-name symbol))
                  (safe-package safe-package))))
     new-symbol))
-
-(defun make-safe-package (name)
-  "Should make a totally independent package. All normal safe stuff should be in this by default.
-
-Some questions to consider:
-  - Is it safe to set *readtable* in the package on creation. By this is it possible for a malicious user to modify this?
-    - What happens when I set it to nil?
-    - Can I replace colon-reader with another more permissive function? If so how?
-  - Is it safe to read using a read function defined in this package? My off the cuff guess is 'not a good idea'. Too much chance for iffy behavior unless it can be proven safe."
-  (with-package (make-empty-package name)
-    (cl::use-package nisp-safe::*prepared-safe-packages*)
-    *package* ) ) 
