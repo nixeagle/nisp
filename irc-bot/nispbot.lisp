a67856ff4c8ba455e4b5c3e984c545c11af908bd
James S <i@nixeagle.org>
2010-01-23 22:19:11 +0000

    add read-sequence-until generic and one method specializing on streams

--- irc-bot/nispbot.lisp
+++ irc-bot/nispbot.lisp
@@ -206,6 +206,25 @@ This does _not_ cause [ ] \\ ~ to be translated to { } | ^."
 
 (defclass raw-irc-protocol-line (raw-protocol-line maximum-message-length) ())
 
+(defgeneric read-sequence-until (object target &key limit non-blocking)
+  (:documentation "Read OBJECT up to TARGET.
+
+Stop reading OBJECT if LIMIT is reached."))
+
+;;; Guidance taken from cl-irc on this implentation. However they use
+;;; iterate and treat this as a function not a generic method/methods.
+(defmethod read-sequence-until ((char-stream stream) (target character)
+                                &key limit non-blocking)
+  "Read CHAR-STREAM upto TARGET."
+  ;; Need to actually handle LIMIT and NON-BLOCKING or handle
+  ;; NON-BLOCKING as part of the stream type.
+  (declare (type (or positive-fixnum nil) limit)
+           (type boolean non-blocking)
+           (ignore limit non-blocking))
+  (iter (for char :in-stream char-stream :using #'read-char-no-hang)
+        (until (or (null char) (char= target char)))
+        (collect char)))
+
 (defpackage #:nisp-user
   (:use :cl :usocket :nisp.irc :nisp.irc-types :nisp.util-types
         :iterate))
