a9eb871be6b69aa097509fdbe2b1bbbe29372f00
James S <i@nixeagle.org>
2010-01-25 02:26:52 +0000

    add normalize-irc-command

--- fbi/irc-commands.lisp
+++ fbi/irc-commands.lisp
@@ -7,10 +7,19 @@
 
 (in-package :nisp.fbi.irc-commands)
 
+(defun normalize-irc-command (command-string)
+  "Lowercase the COMMAND-STRING for method selection."
+  (declare (type string))
+  (string-downcase command-string))
+
 (defun listen-loop (socket)
   "Listen for input and send it through nisp."
   ;; Broken atm
-  (iter (for sock = (wait-for-input socket :ready-only t))
-        (json-nisp-message (nstring-downcase
-                            (princ-to-string
-                             (json->alist (read-json sock)))))))
+  (iter (for sock = (wait-for-input socket :timeout 36000 :ready-only t))
+        (when sock
+          (json-nisp-message (nstring-downcase
+                              (princ-to-string
+                               (json->alist (read-json (car sock)))))))))
+
+(sb-bsd-sockets:local-abstract-socket
+ )
