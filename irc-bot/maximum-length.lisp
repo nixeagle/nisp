92b17b82247ed12e8f9eb2b53d362b285b198d49
James S <i@nixeagle.org>
2010-01-25 07:54:04 +0000

    document maximum-length-mixin

--- irc-bot/maximum-length.lisp
+++ irc-bot/maximum-length.lisp
@@ -1,17 +1,25 @@
 (defpackage #:nisp.clos.maximum-length
   (:use :cl :nisp.util-types)
   (:export :maximum-length-mixin
-           limited-length-mixin
+           :limited-length-mixin
            :maximum-length
            #:valid-lenth-p))
 (in-package :nisp.clos.maximum-length)
-(defclass maximum-length-mixin () ())
+
+(defclass maximum-length-mixin () ()
+  (:documentation "Object expected to respond to and setf a maximum-length.
+
+How or where the slot (if there even is one) is stored does not matter."))
+
 (defclass limited-length-mixin (maximum-length-mixin) ()
     (:documentation "Represents objects where it makes sense to call valid-length-p.
 This is the set of all objects that decompose to a string."))
 
 (defgeneric maximum-length (object)
   (:method ((object maximum-length-mixin))
+    "Subclassing MAXIMUM-LENGTH-MIXIN requires this method to be defined.
+
+Additionally you _must_ return a number."
     (error "Method on mixin class invalid.")))
 
 (defgeneric (setf maximum-length) (length object)
