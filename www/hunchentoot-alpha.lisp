0364338e9f78904645a2c92643c736a045f88710
James S <i@nixeagle.org>
2010-01-26 19:14:15 +0000

    START-ALPHA-ACCEPTOR! is a destructive function that starts a new acceptor or makes a new one and stores it in *alpha-acceptor* if the last one has already been shutdown.

--- www/hunchentoot-alpha.lisp
+++ www/hunchentoot-alpha.lisp
@@ -17,6 +17,16 @@
   ;; and then call (start *alpha-acceptor*).
   "Default acceptor.")
 
-(defun start-alpha-site ())
+(defun start-alpha-acceptor! ()
+  "  Startup ACCEPTOR.
+
+  When ACCEPTOR has already been shutdown, a brand new instance is
+  made and that new instance is used instead."
+  ;; ACCEPTOR used to be an optional argument. For the time being this
+  ;; option has been removed to simplify starting and stopping just one
+  ;; acceptor.
+  (when (hunchentoot::acceptor-shutdown-p *alpha-acceptor*)
+    (setq *alpha-acceptor* (make-instance 'acceptor :port *alpha-port*)))
+  (start *alpha-acceptor*))
 
 ;;; End hunchentoot-alpha.lisp (for magit/git)
