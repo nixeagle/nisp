e5f130be0363a1a369b4bce620054970f0c3681f
James S <i@nixeagle.org>
2010-01-28 02:20:09 +0000

    make get-normal-translate-data-node more robust, plus simple fixes and typo corrections

--- irc-bot/google.lisp
+++ irc-bot/google.lisp
@@ -48,12 +48,14 @@ Note that the translation text is stored in a child node of NODE."
   "The translation in NODE's data field is returned.
 
 NODE should be of a short_text html/css class."
-  (declare (type (and stp:node (satisfies translate-short-text-p)) node))
-  (stp:data (car (stp:list-children (car (stp:list-children node))))))
+  (declare (type stp:node node))
+  (if (typep node 'stp:text)
+      (stp:data node)
+      (get-normal-translate-data-node (car (stp:list-children node)))))
 
 (defun get-suggestion-translate-data-node (node)
   "The translation in NODE's data field is returned."
-  (declare (type (and stpp:node (satisfies translate-suggestion-p)) node))
+  (declare (type (and stp:node (satisfies translate-suggestion-p)) node))
   (stp:data (car (stp:list-children node))))
 
 (defun simple-translate (text from to)
