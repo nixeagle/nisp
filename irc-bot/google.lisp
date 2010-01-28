189c2d860bd412465aacdf71c0f20f9d7dc2c286
James S <i@nixeagle.org>
2010-01-28 01:07:27 +0000

    add GET-NORMAL-TRANSLATE-DATA-NODE - fish out the translation text and return it.

--- irc-bot/google.lisp
+++ irc-bot/google.lisp
@@ -44,6 +44,15 @@ Note that the translation text is stored in a child node of NODE."
   (declare (type string xml-string))
   (chtml:parse xml-string (cxml-stp:make-builder)))
 
+(defun get-normal-translate-data-node (node)
+  "NODE's data field is returned.
+
+try should be of a short_text html/css class."
+  (declare (type stp:node node))
+  (let ((data (car (stp:list-children (car (stp:list-children node))))))
+    (unless data (error "No data node found, likely a language conversion failed to work. Make sure things like es -> jp actually produce a meaningful translation."))
+    (stp:data data)))
+
 (defun simple-translate (text from to)
   "Translate TEXT FROM a language TO another language."
   (declare (type string text from to))
