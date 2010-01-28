3013a0fb374502272fea8afd8e9a9b9ee856419f
James S <i@nixeagle.org>
2010-01-28 01:11:37 +0000

    update docstring of GET-NORMAL-TRANSLATE-DATA-NODE and check the input type better.

--- irc-bot/google.lisp
+++ irc-bot/google.lisp
@@ -45,10 +45,10 @@ Note that the translation text is stored in a child node of NODE."
   (chtml:parse xml-string (cxml-stp:make-builder)))
 
 (defun get-normal-translate-data-node (node)
-  "NODE's data field is returned.
+  "The translation in NODE's data field is returned.
 
-try should be of a short_text html/css class."
-  (declare (type stp:node node))
+NODE should be of a short_text html/css class."
+  (declare (type (and stp:node (satisfies translate-short-text-p)) node))
   (let ((data (car (stp:list-children (car (stp:list-children node))))))
     (unless data (error "No data node found, likely a language conversion failed to work. Make sure things like es -> jp actually produce a meaningful translation."))
     (stp:data data)))
