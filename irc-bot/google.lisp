dd28365ec6d2cdb220d5aef2e1ecad5206d2136c
James S <i@nixeagle.org>
2010-01-28 01:16:59 +0000

    we don't really need to assert anymore in GET-NORMAL-TRANSLATE-DATA-NODE.
    The type declaration is enough in sbcl, and this behavior should not
    need checking in any other lisp if it works here.

--- irc-bot/google.lisp
+++ irc-bot/google.lisp
@@ -49,13 +49,11 @@ Note that the translation text is stored in a child node of NODE."
 
 NODE should be of a short_text html/css class."
   (declare (type (and stp:node (satisfies translate-short-text-p)) node))
-  (let ((data (car (stp:list-children (car (stp:list-children node))))))
-    (unless data (error "No data node found, likely a language conversion failed to work. Make sure things like es -> jp actually produce a meaningful translation."))
-    (stp:data data)))
+  (stp:data (car (stp:list-children (car (stp:list-children node))))))
 
 (defun get-suggestion-translate-data-node (node)
   "The translation in NODE's data field is returned."
-  (declare (type (and stp:node (satisfies translate-suggestion-p)) node))
+  (declare (type (and stpp:node (satisfies translate-suggestion-p)) node))
   (stp:data (car (stp:list-children node))))
 
 (defun simple-translate (text from to)
