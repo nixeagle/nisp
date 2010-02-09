5fba36cd8c7030d2c1596e090d5dd3328476d93d
James S <i@nixeagle.org>
2010-02-09 04:54:52 +0000

    define a test macro: WITH-FBI-SOCKET and add DEFINE-NEW-SUITE.

--- fbi/tests.lisp
+++ fbi/tests.lisp
@@ -1,7 +1,19 @@
 (in-package :nisp.fbi.json-classes)
 
-(def-suite root)
+(define-new-suite :nisp-eos-root)
+
+(def-suite root :in :nisp-eos-root)
 (test (make-json-type-signature :suite root)
   (with-fbound (make-json-type-signature)
     ('((a . b) (c . d))) '(a c)))
+
+
+(defmacro with-fbi-socket ((sock &optional
+                                 (host "danopia.net")
+                                 (port 5348)) &body body)
+  `(let ((sock ',sock))
+     (unwind-protect (progn
+                       (setq sock (json-socket-connect ,host ,port))
+                       ,@body)
+       (close (socket-stream sock) :abort t))))
 
