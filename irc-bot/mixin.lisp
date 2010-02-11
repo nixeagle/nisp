662db71d8f27135062ec3b9cf3f5341a0cfdd6bc
James S <i@nixeagle.org>
2010-02-11 22:22:13 +0000

    Add CONNECTION-MIXIN, SENDER-MIXIN, TARGET-MIXIN, MESSAGE-MIXIN.
    These 4 will let us abstract away from a particular protocol inside
    HANDLE-COMMAND and other generic functions.

--- irc-bot/mixin.lisp
+++ irc-bot/mixin.lisp
@@ -8,3 +8,15 @@ This is different then any handle/nick/username/email. A person's
 identity should be the same across whatever protocols we operate
 over."))
 
+
+(defclass connection-mixin () ()
+  (:documentation "Protocol agnostic interface specification."))
+
+(defclass sender-mixin () ())
+
+(defclass target-mixin () ())
+
+(defclass message-mixin () ())
+
+
+;;; END
