08a07f44e4cdd396c1773f9c585d6a59c6492989
James S <i@nixeagle.org>
2010-02-18 06:04:24 +0000

    test cases for preprocess-arglist

--- irc-bot/tree-funcallable-class-tests.lisp
+++ irc-bot/tree-funcallable-class-tests.lisp
@@ -20,3 +20,17 @@
     ("it") (list (find-symbol "IT" +tree-symbols-package+))
     ("it it2") (list (find-symbol "IT" +tree-symbols-package+)
                      (find-symbol "IT2" +tree-symbols-package+))))
+
+
+(test (preprocess-arglist
+       :suite tree-funcallable
+       :depends-on ensure-tree-symbol)
+  "Expect only first argument to ever be modified in the return value."
+  (let ((expect '((NISP.I.COMMAND-ARGUMENT-SYMBOLS::ALPHA
+                   NISP.I.COMMAND-ARGUMENT-SYMBOLS::NIKURL)
+                  1 2 3 4 "args")))
+    (with-fbound (preprocess-arglist)
+      (#'test-tree-generic-function '("Alpha nikurl" 1 2 3 4 "args"))
+      expect
+      (#'test-tree-generic-function '((alpha nikurl) 1 2 3 4 "args"))
+      expect)))
