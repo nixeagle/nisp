91d10b0d8d65e63f51f8c859b395a85871b7d349
James S <i@nixeagle.org>
2010-02-23 07:01:05 +0000

    update for new website stuff hopefully.

--- www/hunchentoot-alpha.lisp
+++ www/hunchentoot-alpha.lisp
@@ -1,19 +1,19 @@
 (in-package :cl-user)
-(defpackage #:nisp.hunchentoot.alpha-site
+(defpackage #:nisp.www
   (:use :cl :hunchentoot))
-(in-package :nisp.hunchentoot.alpha-site)
+(in-package :nisp.www)
 
-(defparameter *alpha-port* 1337
+(defparameter *nisp-port* 80
   "Default port for alpha-site.")
 
-(defclass alpha-site-acceptor (acceptor)
+(defclass nisp-site-acceptor (acceptor)
   ()
-  (:default-initargs :port *alpha-port*)
+  (:default-initargs :port *nisp-port*)
   (:documentation "Alpha site has its own acceptor so that testing hooks
 can specialize on the acceptor class and not mess with the normal
 hunchentoot acceptor."))
 
-(defvar *alpha-acceptor* (make-instance 'alpha-site-acceptor)
+(defvar *nisp-acceptor* (make-instance 'nisp-site-acceptor)
   ;; Note there is an oddity that I cannot stop one of these and then
   ;; start it and expect it to work as I would normally intend. Calling
   ;; REINITIALIZE-INSTANCE on the instance did not do anything useful
@@ -24,12 +24,12 @@ hunchentoot acceptor."))
   ;; and then call (start *alpha-acceptor*).
   "Default acceptor.")
 
-(defvar *alpha-last-request* nil
+(defvar *nisp-last-request* nil
   "Contents of the last hunchentoot request.")
 
 ;;; Destructive toplevel settings to hunchentoot
-(setq *message-log-pathname* "/home/james/n/www/message.log"
-      *access-log-pathname* "/home/james/n/www/access.log"
+(setq *message-log-pathname* "/tmp/sbcl/message.log"
+      *access-log-pathname* "/tmp/sbcl/access.log"
       *lisp-errors-log-level* :info     ;Default is :error
       *lisp-warnings-log-level* :info   ;Default is :warning
 
@@ -38,7 +38,7 @@ hunchentoot acceptor."))
       *show-lisp-errors-p* t)
 
 
-(defun start-alpha-acceptor! ()
+(defun start-nisp-acceptor! ()
   "  Startup ACCEPTOR.
 
   When ACCEPTOR has already been shutdown, a brand new instance is
@@ -46,25 +46,24 @@ hunchentoot acceptor."))
   ;; ACCEPTOR used to be an optional argument. For the time being this
   ;; option has been removed to simplify starting and stopping just one
   ;; acceptor.
-  (when (hunchentoot::acceptor-shutdown-p *alpha-acceptor*)
-    (setq *alpha-acceptor* (make-instance 'alpha-site-acceptor)))
-  (start *alpha-acceptor*))
+  (when (hunchentoot::acceptor-shutdown-p *nisp-acceptor*)
+    (setq *nisp-acceptor* (make-instance 'nisp-site-acceptor)))
+  (start *nisp-acceptor*))
 
 ;;; Modifications/around methods on hunchentoot generics.
-(defmethod handle-request :before ((acceptor alpha-site-acceptor) request)
+(defmethod handle-request :before ((acceptor nisp-site-acceptor) request)
   "Set *ALPHA-LAST-REQUEST* to REQUEST."
-  (setq *alpha-last-request* request)
+  (setq *nisp-last-request* request)
   #+ () (setq *alpha-acceptor* *acceptor*))
 
 
 ;;; dispatching things ------------------------------
 (defun default-handler ()
   "Called if nothing else matches."
-  (log-message :info "Default dispatch called on alpha-site, script: ~A"
+  (log-message :info "Default dispatch called on nisp-site, script: ~A"
                (script-name*))
   "Nothing here!")
 
 (setq *default-handler* 'default-handler)
 
 ;;; End hunchentoot-alpha.lisp (for magit/git)
-
