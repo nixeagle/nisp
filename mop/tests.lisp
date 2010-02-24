7cbe796fe905878314a4cf0c1878a5f50d55713e
James S <i@nixeagle.org>
2010-02-24 04:10:40 +0000

    try clearing out some irrelevant stuff...

--- mop/tests.lisp
+++ mop/tests.lisp
@@ -1,5 +1,12 @@
 (in-package :nisp.mop-simple)
+(define-new-suite :nisp-eos-root)
 (def-suite root :in :nisp-eos-root)
 
-(in-package :nisp.mop)
-(def-suite root :in :nisp-eos-root)
+(test (class-slots :suite root)
+  "Result of `class-slots' should be list of `standard-effective-slot-definition'."
+  (mapc (lambda (class)
+          (is (typep
+               (car (class-slots class)) '
+               closer-mop:standard-effective-slot-definition)))
+        (list 'standard-class
+              "STANDARD-CLASS")))
