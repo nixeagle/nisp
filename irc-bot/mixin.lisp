7be54f4a6dbc522d539b37404a31d206a6b1acb7
James S <i@nixeagle.org>
2010-03-01 23:59:11 +0000

    Add ABSTRACT-DATA-SOURCE and ABSTRACT-DATA-SINK.
    These cover the medium a message came from and the medium a message will
    be sent to.

--- irc-bot/mixin.lisp
+++ irc-bot/mixin.lisp
@@ -15,13 +15,27 @@ over."))
 In principle any connection class derived from usocket that implements all
 methods specialized on this object is a complient interface."))
 
-;;; Next 3 not well specified atm, will be working on this
+;;; Next 5 not well specified atm, will be working on this
 ;;; soon. 01-03-2010
 (defclass abstract-from () ()
   (:documentation "Generic information about a sender."))
 
 (defclass abstract-to () ()
   (:documentation "Generic information about a recipient/target/channel"))
+
+(defclass abstract-data-source () ()
+  (:documentation "Generic information on the _source_ of a message.
+
+This is different from `abstract-from' in that here we care about how we
+got the message, not who or what sent it."))
+
+(defclass abstract-data-sink () ()
+  (:documentation "Generic information about destination protocol/service.
+
+This is different from `abstract-to' in that here we care about where a
+  message is routed to, as opposed to who or what is to eventually receive
+  the message. The term 'sink' means what medium a message will be
+  convoyed over to reach its destination as specified by `abstract-to'."))
 
 (defclass abstract-message-content () ()
   (:documentation "Message contents/information"))
