4f0468ca5ba60ee21cc6fc3b8336176f23b2e54d
James S <i@nixeagle.org>
2010-03-01 22:30:56 +0000

    Amend and document mixin classes
    TARGET-MIXIN -> TO-MIXIN
    SENDER-MIXIN -> FROM-MIXIN
    
    TARGET-MIXIN and SENDER-MIXIN should no longer be used.

--- irc-bot/mixin.lisp
+++ irc-bot/mixin.lisp
@@ -10,13 +10,29 @@ over."))
 
 
 (defclass connection-mixin () ()
-  (:documentation "Protocol agnostic interface specification."))
+  (:documentation "Protocol agnostic interface specification.
 
-(defclass sender-mixin () ())
+In principle any connection class derived from usocket that implements all
+methods specialized on this object is a complient interface."))
 
-(defclass target-mixin () ())
+;;; Next 3 not well specified atm, will be working on this
+;;; soon. 01-03-2010
+(defclass from-mixin () ()
+  (:documentation "Generic information about a sender."))
 
-(defclass message-mixin () ())
+(defclass to-mixin () ()
+  (:documentation "Generic information about a recipient/target/channel"))
+
+(defclass message-mixin () ()
+  (:documentation "Message contents/information"))
+
+
+
+;;; OBSOLETE - replaced by from-mixin and to-mixin
+(defclass sender-mixin (from-mixin) ())
+(defclass target-mixin (to-mixin) ())
+
+
 
 
 ;;; END
