438ee506a3978eab47dfc601a5fbf783892bed80
James S <i@nixeagle.org>
2010-03-06 06:00:59 +0000

    Update USER.LISP, mostly to add with-fbound and eos, plus eval-in-emacs

--- user.lisp
+++ user.lisp
@@ -5,6 +5,8 @@
 (defpackage #:nisp.user
   (:use :cl
         ;; implentation independent sockets
+        :with-fbound
+        :eos
         :usocket
         :drakma)                         ;http-request
   (:nicknames :nu :~))
@@ -21,5 +23,35 @@
        (values
         ,form
         (count #\Newline ,s)))))
+(defun ensure-no-leading-single-quote (string)
+  "Append a leading ' to STRING if there is not already one."
+  (declare (type string string))
+  (if (char= (char string 0) #\')
+      (subseq string 1)
+      string))
 
+(define-condition nisp-user-error (error)
+  ())
+
+(define-condition package-not-found-error (nisp-user-error)
+  ((package :initarg :package :reader package-not-found-error-package))
+  (:report (lambda (condition stream)
+             (format stream "Package ~A is not loaded"
+                     (package-not-found-error-package condition)))))
+
+(defgeneric eval-in-emacs (form &optional nowait))
+(defmethod eval-in-emacs ((form string) &optional nowait)
+  (eval-in-emacs (read-from-string (ensure-no-leading-single-quote form))
+                 nowait))
+(let (eval-in-emacs)
+  (defmethod eval-in-emacs :before (form &optional nowait)
+    (declare (ignore nowait))
+    (unless eval-in-emacs
+      (let ((eval-in-emacs-symbol (find-symbol "EVAL-IN-EMACS" :swank)))
+        (if eval-in-emacs-symbol
+            (setq eval-in-emacs (symbol-function eval-in-emacs-symbol))
+            (error (make-condition 'package-not-found-error
+                                   :package :swank))))))
+  (defmethod eval-in-emacs (form &optional nowait)
+    (funcall eval-in-emacs form nowait)))
 ;;; END
