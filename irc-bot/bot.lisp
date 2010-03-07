4ec4a90d46b05e6db8c8579c6ea98df42fca7855
James S <i@nixeagle.org>
2010-03-07 07:39:40 +0000

    All kinds of fun github commands
    This uses our new routing system!

--- irc-bot/bot.lisp
+++ irc-bot/bot.lisp
@@ -518,6 +518,92 @@ methods that support this."))
   (reply (~::eval-in-emacs "(format-seconds \"%Y, %D, %H, %M, %z%S\"
                  (float-time (or (current-idle-time) '(0 0 0))))")))
 
+
+(define-simple-command beta
+  (network-tree::next-node))
+;;; Not going to mess with this
+#+ () (define-simple-command beta-nikurl
+  (reply (car (cl-ppcre:all-matches-as-strings
+               "http\\S+"
+               (drakma:http-request
+                (concatenate 'string
+                             "http://nik.im/api_create.php?url="
+                             (remove #\Space (network-tree::remaining-parameters))))))))
+
+(flet ((github-apply (function string)
+         (apply function (github::parse-github-repository-notation string))))
+  (define-simple-command github
+    (network-tree::next-node))
+  (define-simple-command github-show
+    (network-tree::next-node))
+  (define-simple-command github-show-followers
+    (reply (cl-github:show-followers (network-tree::remaining-parameters))))
+  (define-simple-command github-show-following
+    (reply (cl-github:show-following (network-tree::remaining-parameters))))
+  (define-simple-command github-show-repositories
+    (reply
+     (mapcar #'cl-github::repository-name
+             (cl-github::show-user-repositories (network-tree::remaining-parameters)))))
+  (define-simple-command github-show-watched
+    (network-tree::next-node))
+  (define-simple-command github-show-watched-repositories
+    (reply
+     (set-difference
+      (mapcar #'github::github-repository-notation
+              (github::watched-repositories (network-tree::remaining-parameters)))
+      (mapcar #'cl-github::github-repository-notation
+              (cl-github::show-user-repositories (network-tree::remaining-parameters)))
+      :test #'equal)))
+  (defmethod one-line-description ((repo github::repository))
+    (flet ((format-fork ()
+             (if (github:repository-fork-p repo)
+                 " (fork)"
+                 ""))
+           (format-people (total)
+             (case total
+               (1 "1 person")
+               (0 "nobody")
+               (t (format nil "~A people" total))))
+           (format-issues (total)
+             (case total
+               (1 "1 open issue")
+               (0 "no open issues")
+               (t (format nil "~A open issues" total)))))
+      (format nil
+              "~A's ~A~A <~A> is watched by ~A, forked by ~A, has ~A, and described as: ~A"
+              (string-capitalize (github:repository-owner repo))
+              (github:repository-name repo)
+              (format-fork)
+              (github:repository-url repo)
+              (format-people (github:repository-watchers-count repo))
+              (format-people (github:repository-forks-count repo))
+              (format-issues (github:repository-open-issues-count repo))
+              (github:repository-description repo))))
+
+  (define-simple-command github-show-repository
+    (reply (one-line-description
+            (github-apply #'github::show-repository
+                          (network-tree::remaining-parameters)))))
+
+  (define-simple-command github-show-collaborators
+    (reply (github-apply #'github::show-collaborators
+                         (network-tree::remaining-parameters))))
+
+  (define-simple-command github-show-tags
+    (reply (mapcar #'car
+                   (github-apply #'github::show-tags
+                                 (network-tree::remaining-parameters)))))
+
+  (define-simple-command github-show-languages
+    (reply (princ-to-string (github-apply #'github::show-languages
+                                          (network-tree::remaining-parameters)))))
+
+  (define-simple-command github-show-branches
+    (reply (format nil "~{~A~^, ~}"
+            (mapcar #'car
+                    (github-apply #'github::show-branches
+                                  (network-tree::remaining-parameters)))))))
+
 (defmethod handle-nisp-command ((tree (eql "say"))
                                 (source abstract-data-source)
                                 (user abstract-user)
