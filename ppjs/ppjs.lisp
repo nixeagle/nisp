d3775e48bbc43213dbd267c901a637ff31f8c235
James S <i@nixeagle.org>
2010-03-14 19:11:27 +0000

    Everything is back to mostly working again
    defun still is not complete, needs to handle more then one statement
    inside it without appending a "return" to each statement.
    
    the various math ops don't have sane error guards, the ones that do don't
    always do them correctly because we are looking at the list in a different
    state then the original code.

--- ppjs/ppjs.lisp
+++ ppjs/ppjs.lisp
@@ -11,13 +11,15 @@
                                       (prin1-to-string (read-from-string (nisp.i::remaining-parameters))))))))))
 (defvar *recursivep* nil)
 (defvar *statementp* nil)
-(defvar *function-level* 0)
+(defvar *top-block-p* nil)
+(defparameter *block-prefix* "    ")
 
-(defun call-wrap-parens (stream thunk)
-  (when *recursivep* (princ #\( stream))
-  (let ((*recursivep* t))
-    (funcall thunk))
-  (when *recursivep* (princ #\) stream)))
+(defun call-wrap-parens (*standard-output* thunk)
+  (pprint-logical-block (nil nil)
+    (when *recursivep* (princ #\())
+    (let ((*recursivep* t))
+      (funcall thunk))
+    (when *recursivep* (princ #\)))))
 
 (defmacro wrap-parens (stream &body body)
   `(call-wrap-parens ,stream (lambda () ,@body)))
@@ -25,30 +27,40 @@
 (defun call-wrap-statement (*standard-output* thunk)
   (let ((*statementp* t))
     (pprint-indent :block 0)
-    (funcall thunk)
-    (unless *recursivep*
-      (write-char #\;)
+    (funcall thunk))
+  (unless *statementp*
+    (write-char #\;)
+    (unless *top-block-p*
       (pprint-newline :linear))))
 (defmacro wrap-statement (stream &body body)
   `(call-wrap-statement ,stream (lambda () ,@body)))
 
 (defun wrap-function (*standard-output* list)
-  (pprint-logical-block (nil list)
-    (pprint-pop)                        ;Ignore defun
-    (pprint-indent :current 0)
-    (princ "function ")
-    (write (pprint-pop))
-    (write-char #\Space)
-    (pprint-function-lambda-list *standard-output* (pprint-pop))
-    (write-char #\Space)
-    (wrap-braces *standard-output* (cdddr list))))
+  (let ((*recursivep* nil)
+        (*statementp* nil))
+    (pprint-logical-block (nil list)
+      (pprint-pop)                      ;Ignore defun
+      (princ "function ")
+      (write (pprint-pop))
+      (write-char #\Space)
+      (pprint-function-lambda-list *standard-output* (pprint-pop))
+      (write-char #\Space)
+      (wrap-braces *standard-output*
+        (pprint-logical-block (*standard-output* (cdddr list))
+          (loop
+             (pprint-exit-if-list-exhausted)
+             (let ((next (pprint-pop)))
+               (princ "return ")
+               (write next))))))))
 
-(defparameter *block-prefix* "    ")
 (defun call-wrap-braces (*standard-output* thunk)
   (write-char #\{)
   (pprint-newline :mandatory)
-  (princ *block-prefix*)
-  (funcall thunk)
+  (let ((*top-block-p* t))
+    (pprint-logical-block (*standard-output* nil)
+      (princ *block-prefix*)
+      (funcall thunk)))
+  (pprint-newline :linear)
   (write-char #\}))
 (defmacro wrap-braces (stream &body body)
   `(call-wrap-braces ,stream (lambda () ,@body)))
@@ -62,45 +74,51 @@
 (defmacro noppc (arg stream)
   `(let ((*print-pretty* nil))
      (princ ,arg ,stream)))
+(defun call-print-infix-op (*standard-output* list printing-function)
+  (pprint-logical-block (*standard-output* list)
+    (let ((op (pprint-pop)))
+      (loop
+         (let ((next (pprint-pop)))
+           (funcall printing-function next))
+         (pprint-exit-if-list-exhausted)
+         (print-op op)))))
+(defmacro with-print-infix-op ((var list &optional (stream '*standard-output*)) &body body)
+  `(call-print-infix-op ,stream ,list
+                        (lambda (,var) ,@body)))
 (defun pprint-+ (*standard-output* list)
-  (wrap-braces *standard-output*
-    (wrap-statement *standard-output*
-      (pprint-logical-block (*standard-output* list)
-        (let ((op (pprint-pop)))
-          (loop
-             (write (pprint-pop))
-             (pprint-exit-if-list-exhausted)
-             (print-op op)))))))
+  (wrap-statement *standard-output*
+    (wrap-parens *standard-output*
+      (with-print-infix-op (next list)
+        (write (or next 0))))))
 
-(defun pprint-* (stream list)
-  (wrap-statement stream (cdr list)
-                  #+ () (if (not (null (cdr list)))
-                      (wrap-parens stream
-                        (funcall (formatter "~{~S~^ * ~}") stream))
-                      (princ 1 stream))))
+(defun pprint-* (*standard-output* list)
+  (wrap-statement *standard-output*
+    (wrap-parens *standard-output*
+      (with-print-infix-op (next list)
+        (write (or next 1))))))
 
-(defun pprint-- (stream list)
-  (wrap-statement stream (cdr list)
-                #+ ()  (case (length)
-                    (1 (princ "-" stream)
-                       (prin1 (cadr list) stream))
-                    (0 (error "You cannot subtract with 0 arguments!"))
-                    (otherwise
-                     (wrap-parens stream
-                       (funcall (formatter "~{~S~^ - ~}") stream (cdr list)))))))
+(defun pprint-- (*standard-output* list)
+  (wrap-statement *standard-output*
+    (wrap-parens *standard-output*
+      (with-print-infix-op (next list)
+        (case next
+          (1 (princ "-")
+             (write next))
+          (null (error "You cannot subtract with 0 arguments!"))
+          (otherwise
+           (write next)))
+        ))
+    ))
 
-(defun pprint-/ (stream list)
-  (wrap-statement stream (cdr list)#+ ()
-                  (case (length (cdr list))
-                    (1 (when (and (numberp (cadr list)) (zerop (cadr list)))
-                         (error 'division-by-zero :operation 'pprint-/
-                                :operands (list 1 (cadr list))))
-                       (princ "1/" stream)
-                       (prin1 (cadr list) stream))
-                    (0 (error "You cannot divide by 0 arguments."))
-                    (otherwise
-                     (wrap-parens stream
-                       (funcall (formatter "~{~S~^ / ~}") stream (cdr list)))))))
+(defun pprint-/ (*standard-output* list)
+  (wrap-statement *standard-output*
+    (wrap-parens *standard-output*
+      (with-print-infix-op (next list)
+        (case next
+          (0 (error 'division-by-zero :operation 'pprint-/
+                    :operands (list 1 (cadr list))))
+          (null (error "You cannot divide by 0 arguments."))
+          (otherwise (write next)))))))
 
 (defun pprint-defun (stream list)
   (wrap-function stream list))
@@ -116,11 +134,12 @@
 (defparameter *js-table*
   (let ((*print-pprint-dispatch* (copy-pprint-dispatch nil)))
     (set-pprint-dispatch 'symbol 'pprint-symbol)
+    (set-pprint-dispatch '(cons (member defun)) 'pprint-defun)
     (set-pprint-dispatch '(cons (member +)) 'pprint-+)
     (set-pprint-dispatch '(cons (member -)) 'pprint--)
     (set-pprint-dispatch '(cons (member *)) 'pprint-*)
     (set-pprint-dispatch '(cons (member /)) 'pprint-/)
-    (set-pprint-dispatch '(cons (member defun)) 'pprint-defun)
+
     *print-pprint-dispatch*))
 (defun js-pprint-table ()
   *js-table*)
