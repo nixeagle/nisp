1bcfe784e3bf80914ac8afe94b3ab2edf1c23bfc
James S <i@nixeagle.org>
2010-04-05 03:29:58 +0000

    Document NISP-STANDARD method combination

--- util/nisp-standard-combination.lisp
+++ util/nisp-standard-combination.lisp
@@ -67,6 +67,58 @@ will be grouped together."
 
 (define-method-combination nisp-standard (&key hook)
   ((methods *))
+  "Alternate, more advanced standard method combination.
+
+There are 6 different kinds of qualified methods. The 4 standard
+qualifiers are `:around' `:before' `:after' and `primary'. These have the
+expected behavior with the exception of `primary' when HOOK is non-nil.
+
+`primary' when HOOK is non-nil is treated as a list of methods to run in
+sequence just as GNU Emacs hooks behave. The value of HOOK should be the
+name of a function to pass the results to. The behavior here is similar to
+the simple-method-combinations specified in the ANSI specification with
+one exception. Specifying additional qualifiers unrelated to any of the
+qualifiers already used as part of this method combination strategy will
+allow multiple methods specialized on the same arguments to be on the hook
+at the same time.
+
+For example if a generic function is specified as:
+
+  (:method-combination nisp-standard :hook list)
+
+All the applicable `primary' methods will run and each will have its
+result placed in a list and returned as the result of the generic-function
+call.
+
+A useful way to get similar behavior to GNU Emacs'
+`run-hook-with-args-until-success' is to define the method combination
+with `:hook' as `or'. Then all methods on the hook will run until one
+returns non-nil. To achieve behavior like
+`run-hook-with-args-until-failure' define the method combination with
+`:hook' set to `and' and all methods on the hook will then run until one
+returns nil.
+
+Do not forget that when HOOK is specified, primary methods may have the
+same specializers so long as they have additional unrelated
+qualifiers. The following two method defintions are valid in this case.
+
+  (defmethod foo (arg1 arg2) 1)
+  (defmethod foo :different-qualifier (arg1 arg2) 2)
+
+Finally two additional qualifiers are supported:
+
+ + `:meta-around' uses the same principle that asdf uses for their special
+    around qualifier. This allows library code to use around methods
+    without hindering the user's ability to add their own around
+    methods. These methods get called before any `:around', `:before',
+    `:after', or `primary' methods and are called in most-specific-first
+    order, just as `:around' methods are.
+
+ + `:defaulting' methods are mostly useful for specifying defaults for an
+    entire generic function. These are called before any other methods,
+    and are run in least-specific-first order, the _opposite_ of
+    `:meta-around' and `:around' methods. The idea for this is taken from
+    WRAPPING-STANDARD."
   (multiple-value-bind (primary defaulting meta-around
                                 around before after)
       (collect-normal-combinations methods
