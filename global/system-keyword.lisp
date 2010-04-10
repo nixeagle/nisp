c86e3e6899bd74faa2b05ece4cba6e9ae9c0dc77
James S <i@nixeagle.org>
2010-04-10 00:53:23 +0000

    rename production vps, and push onto *keywords*

--- global/system-keyword.lisp
+++ global/system-keyword.lisp
@@ -2,10 +2,13 @@
 
 (defparameter +systems+
   '(("lappy" . :nisp-devel)
-    ("box" . :nisp-production))
+    ("box" . :nisp-vps))
   "Alist of common systems that nixeagle runs nisp on. These are lookups
   to get to a standardized keyword in some cases where these are
   required.")
+
+(pushnew (cdr (assoc (machine-instance) +systems+ :test #'string=))
+         *features*)
 
 (defun system-keyword ()
   "Return a keyword depending on where we are running. If the system is
