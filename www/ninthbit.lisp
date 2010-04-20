3bc9dd26dc48ebb02416ff34db91f5f429a2fb13
James S <i@nixeagle.org>
2010-04-20 18:41:02 -0400

    test handler for printing 9b channels

--- www/ninthbit.lisp
+++ www/ninthbit.lisp
@@ -12,9 +12,19 @@
 (defparameter *9b*
   (hunchentoot-vhost:make-virtual-host "9b"
                                        '(#+nisp-vps "ninthbit.net"
-                                         #+nisp-devel "9b-dev")
+                                         #+nisp-devel "9b-dev"
+                                         #+nisp-devel "nix.ninthbit.net")
                                        :server (primary-acceptor)))
 
 (hunchentoot-vhost:define-easy-virtual-handler *9b*
     (9b-main-page :uri "/") ()
   "Welcome to ninthbit.net. The irc network is at irc.ninthbit.net.")
+
+#+nisp-devel
+(hunchentoot-vhost:define-easy-virtual-handler *9b*
+    (9b-channels-listing :uri "/channels") ()
+  (with-output-to-string (*standard-output*)
+    (mapc (lambda (channel-name)
+            (princ channel-name)
+            (princ "<br>"))
+          (hash-table-keys (irc:channels nisp.i::*devel-bot*)))))
